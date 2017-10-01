64a1ac6c3572abce5863ff9f1cb1fdb6ab7e8a4f
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-01 16:16:26 +0300

    Конвертер расстояний

---
+++ 03. ifs/Конвертер расстояний/Конвертер расстояний/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Конвертер_расстояния
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите расстояние в см");
+            string name = Console.ReadLine();
+            int digit1 = 100;
+            //Расстояние в метрах
+            int digit2 = 10;
+            //Расстояние в дециметрах
+            double sum1 = (double)int.Parse(name) / 100;
+            Console.WriteLine("Расстояние в метрах " + sum1);
+            Console.ReadLine();
+            double sum2 = (double)int.Parse(name) / 10;
+            Console.WriteLine("Расстояние в дециметрах " + sum2);
+            Console.ReadLine();
+        }
+    }
+}
