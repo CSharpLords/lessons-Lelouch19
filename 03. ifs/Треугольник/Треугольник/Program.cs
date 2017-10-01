523b341e8e69ed4c634600a9c8622b369acbc457
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-01 16:56:32 +0300

    Треугольник

---
+++ 03. ifs/Треугольник/Треугольник/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите длину первой стороны");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Напишите длину второй стороны");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Напишите длину третей стороны");
+            int c = int.Parse(Console.ReadLine());
+            //Теорема Пифагора - А в квадрате =
+            if (a*a
+        }
+    }
