45a74d974936662a45fd9f92ca06c82b566a1da4
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-01 16:15:56 +0300

    Винрейт

---
+++ 03. ifs/Винрейт/Винрейт/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Во сколько игр вы сыграли?");
+            int games1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Во скольких играх ты выиграл?");
+            int games2 = int.Parse(Console.ReadLine());
+            double games = (double) games2 / games1;
+            Console.WriteLine("Ваш винрейт = " + games);
+            Console.ReadLine();
+        }
+    }
+}
