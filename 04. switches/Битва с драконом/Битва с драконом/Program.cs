c7c4b845aca363bb30b6c136de6343a53c8929d9
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-08 16:41:22 +0300

    Битва с драконом

---
+++ 04. switches/Битва с драконом/Битва с драконом/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите возраст дракона");
+            int age = int.Parse(Console.ReadLine());
+            if (age < 150)
+            {
+                Console.WriteLine("Введите количество хп.Не больше 100");
+                int hp = int.Parse(Console.ReadLine());
+                if (hp < 50)
+                {
+                    Console.WriteLine("Вы можете убить этого злобного дракона!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("У дракона слишком много хп.Бегите!");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы не можете победить этого зловещего дракона!");
+                Console.ReadLine();
+            }
+ 
+
+        }
+    }
+}
