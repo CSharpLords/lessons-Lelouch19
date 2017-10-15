37c06e4379e2346c3ba5d9ffcb17de63b9586e4c
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-15 15:49:08 +0300

    Наибольшее число(упрощённая версия)

---
+++ 05. switches/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третье число");
+            int c = int.Parse(Console.ReadLine());
+            if (a >= b && a >= c)
+            {
+                Console.WriteLine("Наибольшее число " + a);
+            }
+            else
+            {
+                if (b >= c && b >= c)
+                {
+                    Console.WriteLine("Наибольшее число " + b);
+                }
+
+                if (c >= b && c >= a)
+                {
+                    Console.WriteLine("Наибольшее число " + c);
+                }
+            }
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
