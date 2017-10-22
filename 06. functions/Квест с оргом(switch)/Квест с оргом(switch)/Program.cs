7547be20282ab11dec37b80c723245b46b7415d6
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-10-22 15:22:04 +0300

    Квест с оргом(switch)

---
+++ 06. functions/Квест с оргом(switch)/Квест с оргом(switch)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ты у входа в пещеру, которую охраняет огр. У тебя есть варианты действий:");
+            Console.WriteLine("1. Вступить в бой.");
+            Console.WriteLine("2. Ты отступаешь.");
+            Console.WriteLine("3. Подкупаешь огра.");
+            Console.WriteLine("4. Ищешь обходной путь.");
+            int answer = int.Parse(Console.ReadLine());
+
+           switch (answer){
+               case 1:
+                   Console.WriteLine("Ты погибаешь, так как огр сильнее.");
+                   break;
+               case 2:
+                   Console.WriteLine("Переносишься в соседнюю локацию.");
+                   break;
+               case 3:
+                   Console.WriteLine("Он пропускает тебя внутрь.");
+                   break;
+               case 4:
+                   Console.WriteLine("По пути тебя грабят бандиты.");
+                   break;
+           }
+            Console.ReadLine();
+        }
+
+    }
+}
