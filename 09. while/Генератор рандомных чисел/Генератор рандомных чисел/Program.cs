4ff14f9e204ebfb32bf1f1dc2835a490fac352fa
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-11-12 16:48:53 +0300

    Генератор рандомных числ

---
+++ 09. while/Генератор рандомных чисел/Генератор рандомных чисел/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Random rand = new Random();
+                int n = rand.Next(0, 100000);
+                Console.WriteLine(n);
+                Console.ReadLine();
+            }
+        }
+    }
+}
