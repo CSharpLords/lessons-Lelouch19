7e4745a36a0dcfa6896e65ec3b7fe47b83f234b9
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-11-12 16:48:17 +0300

    While

---
+++ 09. while/While/While/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int second = 0;
+            while (true)
+            {
+                Console.WriteLine(second + 99999999999999);
+                second = second + 1;
+
+            }
+        }
+    }
+}
