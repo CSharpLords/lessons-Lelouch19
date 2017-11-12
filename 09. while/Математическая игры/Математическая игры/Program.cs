3f36d1f4dc3990ea82ddd360fc31f36f06b32219
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-11-12 17:03:36 +0300

    Математическая игра

---
+++ 09. while/Математическая игры/Математическая игры/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int n = rand.Next(0, 100000);
+            Console.WriteLine(n);
+            while (true)
+            {
+                int number = rand.Next(0, 50);
+                Console.WriteLine("ЧИСЛО - " + number);
+                Thread.Sleep(2000);
+                Console.Clear();
+
+                Console.WriteLine("Введите число которое запомнили");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (number == guess)
+                {
+                    Console.WriteLine("Вы вели число верно");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Число не то");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+
+            }
+        }
+    }
+}
