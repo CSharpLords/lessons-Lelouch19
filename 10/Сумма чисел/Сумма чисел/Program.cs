65913e010e8e69b3e05064b2a1a31105fcebb229
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-11-26 16:44:32 +0300

    Сумма чисел

---
+++ 10/Сумма чисел/Сумма чисел/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            while (sum < 4)
+            {
+                Console.WriteLine("Введите число.");
+                int n = int.Parse(Console.ReadLine());
+                sum = sum + 1;
+            }
+            Console.WriteLine("Сумма чисел - " + sum);  
+            Console.ReadLine();
+        }
+    }
+}
