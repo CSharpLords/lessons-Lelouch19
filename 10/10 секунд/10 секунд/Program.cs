b475c629b70f7ae80961a57446a0badb9d058ef3
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-11-26 16:13:22 +0300

    10 секунд

---
+++ 10/10 секунд/10 секунд/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 0;
+            while (true) 
+            {
+                Thread.Sleep(1000);
+                time = time + 1;
+                Console.WriteLine(time);
+                if (time == 10)
+                {
+                    break;
+                }
+            }
+                    Console.ReadLine();
+        }
+    }
+}
