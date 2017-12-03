0c431333b22d38c7c9916c8263d70beb719e82a0
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-03 16:12:25 +0300

    Слово

---
+++ 12. for/Слова/Слова/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВВЕДИТЕ СЛОВО!СРОЧНО!");
+            string Word = Console.ReadLine().ToLower();
+            int End = Word.Length - 1;
+
+            if (Word[0] == Word[End])
+            {
+                Console.WriteLine("Слово идеально!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Не о чём слово...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
