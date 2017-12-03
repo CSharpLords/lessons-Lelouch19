3852d8c49e3d7699dcbacc3553b2ead74f3c3ea0
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-03 15:16:57 +0300

    Таймер с FOR

---
+++ 12. for/Таймер с FOR/Таймер с FOR/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int Time = 0; Time <= 10; Time = Time + 1)
+            {
+                Console.WriteLine("Time - " + Time);
+                Thread.Sleep(500);
+            }
+            Console.ReadLine();
+        }
+    }
+}
