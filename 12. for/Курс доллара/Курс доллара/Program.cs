2e66268dc32a6fd92a7068aa10d25030101b85f4
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-03 16:41:52 +0300

    Курс доллара

---
+++ 12. for/Курс доллара/Курс доллара/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int Dollar = 0; Dollar <= 20; Dollar = Dollar + 1)
+            {
+                int ChangeRate = 58;
+                Console.WriteLine(Dollar + " $ = " + ChangeRate * Dollar);
+            }
+            Console.ReadLine();
+        }
+    }
+}
