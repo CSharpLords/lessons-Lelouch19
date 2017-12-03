19ce8041e7affcc7f681f3002d13102e7433d512
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-03 16:37:34 +0300

    Числа

---
+++ 12. for/Числа/Числа/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int Shaitanka = 10; Shaitanka <= 25; Shaitanka = Shaitanka + 1)        //Shaitanka - это число
+            {
+                Console.WriteLine(Shaitanka + " " + Shaitanka + "." + 4);
+            }
+            Console.ReadLine();
+        }
+    }
+}
