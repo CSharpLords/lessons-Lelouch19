783b5e216ba395fed43418e150556778b35bb4a2
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-03 16:04:53 +0300

    Поиск буквы "и"

---
+++ 12. for/Поиск буквы 'и'/Поиск буквы 'и'/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово.");
+            string Word = Console.ReadLine().ToLower();
+
+
+            for (int letter = 0; letter < Word.Length; letter = letter + 1)
+            {
+                if (Word[letter] == 'и')
+                {
+                    Console.WriteLine("В слове " + Word + " буква 'и' есть!");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+            }
+
+            Console.WriteLine("В слове " + Word + " буква 'и' нет!");
+            Console.ReadLine();
+        }
+    }
+}
