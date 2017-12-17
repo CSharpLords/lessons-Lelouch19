4d77ea7d5885b02da56fc1573d20b717fad7a97a
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-17 16:57:19 +0300

    Умножение чисел в массиве

---
+++ 14. arrays/Умножение всех чисел в массиве/Умножение всех чисел в массиве/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplication = 1;
+            int[] massif = { 12, 1, 97, 1, 1 };
+            for (int n = 0; n < massif.Length; n = n + 1)
+            {
+                multiplication = multiplication * massif[n];
+            }
+            Console.WriteLine("Произведение всех чисел - " + multiplication);
+            Console.ReadLine();
+        }
+    }
+}
