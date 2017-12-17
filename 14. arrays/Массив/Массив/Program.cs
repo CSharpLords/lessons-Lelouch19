e4e3592cb8f287dfa3d284c4436acb3f5a1e46fd
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-17 16:28:50 +0300

    Массив

---
+++ 14. arrays/Массив/Массив/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = {5, 7, 98, - 7, 100};
+            Numbers[2] = 34;
+            for (int n = 0; n < Numbers.Length; n = n + 1)
+            {
+                Numbers[n] = Numbers[n] * 2;
+                Console.WriteLine(Numbers[n]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
