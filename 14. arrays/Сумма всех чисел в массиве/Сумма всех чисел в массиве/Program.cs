c826d2cc232e244f86ba35617c25413a08b94fd7
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-17 16:53:15 +0300

    Сумма всех чисел в массиве

---
+++ 14. arrays/Сумма всех чисел в массиве/Сумма всех чисел в массиве/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[]massif = {12, 1, 97, 1, 1};
+            for (int n = 0; n < massif.Length; n = n + 1)
+            {
+                sum = sum + massif[n];
+            }
+            Console.WriteLine("Сумма всех чисел - " + sum);
+            Console.ReadLine();
+        }
+    }
+}
