3dbe34316400560aff1611bf9019da1928bc7f35
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-17 16:44:21 +0300

    Деление в массиве

---
+++ 14. arrays/Деление в массиве/Деление в массиве/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] Numbers = { 5, 7, 98, -7, 100 };
+
+            for (int n = 0; n < Numbers.Length; n = n + 1)
+            {
+                double N = Numbers[n] / Numbers[0];
+                Console.WriteLine(N);
+            }
+            Console.ReadLine();
+        }
+    }
+}
