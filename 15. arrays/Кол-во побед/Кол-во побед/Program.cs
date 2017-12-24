7bcca9cb1c354e0f7886b00de0ebe6b4e51f44f6
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 15:24:37 +0300

    Ков-во побед

---
+++ 15. arrays/Кол-во побед/Кол-во побед/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Massif = { 1, 0, 78, 5, 29, 6, 5, 38, 7, 9, 1, 12, 13, 14, 2, 7, 3, 18, 4, 0 };
+            for (int n = 0; n < Massif.Length; n = n + 1)
+            {
+                if (Massif[n] < 3)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
