64f330260fadd9b02de9ced8733d5e1d2c6f3348
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 16:23:47 +0300

    Поиск чисел заканчивающихся на 0

---
+++ 15. arrays/Поиск элементов заканчивающихся на 0/Поиск элементов заканчивающихся на 0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Massif = { 10, 78, 89, -156, 200, 1500};
+            for (int n = 0; n < Massif.Length; n = n + 1)
+            {
+                int Rest = Massif[n] % 10;
+                if (Rest == 0)
+                {
+                    Console.WriteLine(Massif[n]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
