f466a9dba63100e681e87282587819b68b0b443e
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 15:19:22 +0300

    Поиск 5 в массиве

---
+++ 15. arrays/Поиск 5 в массиве/Поиск 5 в массиве/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]Massif = { 98, 89, 462, 5, 0 };
+            for (int n = 0; n < Massif.Length; n = n + 1)
+            {
+                if (Massif[n] == 5)
+                {
+                    Console.WriteLine(n);
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
