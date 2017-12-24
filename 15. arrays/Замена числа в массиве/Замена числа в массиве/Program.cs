59cd24c6a198c7c723df0709b910ba6f0d383112
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 16:39:15 +0300

    Замена числа в массиве

---
+++ 15. arrays/Замена числа в массиве/Замена числа в массиве/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = {0.4, 12, 78, 87.2, 44.4};
+            for (int n = 0; n < numbers.Length; n = n + 1)
+            {
+                if (numbers[n] > 10)
+                {
+                    numbers[n] = Math.Sqrt(numbers[n]);
+                    Console.WriteLine(numbers[n]);
+
+                }
+                else
+                {
+                    Console.WriteLine(numbers[n]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
