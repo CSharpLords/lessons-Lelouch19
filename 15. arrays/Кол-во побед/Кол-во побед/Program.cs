c9dc812f21290ef584f105864f2ce3d9a6b18d08
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 15:30:15 +0300

    Переименовал массив

--- 15. arrays/Кол-во побед/Кол-во побед/Program.cs
+++ 15. arrays/Кол-во побед/Кол-во побед/Program.cs
@@ -9,10 +9,10 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int[] Massif = { 1, 0, 78, 5, 29, 6, 5, 38, 7, 9, 1, 12, 13, 14, 2, 7, 3, 18, 4, 0 };
-            for (int n = 0; n < Massif.Length; n = n + 1)
+            int[] Victories = { 1, 0, 78, 5, 29, 6, 5, 38, 7, 9, 1, 12, 13, 14, 2, 7, 3, 18, 4, 0 };
+            for (int n = 0; n < Victories.Length; n = n + 1)
             {
-                if (Massif[n] < 3)
+                if (Victories[n] < 3)
                 {
                     Console.WriteLine(n);
                 }
