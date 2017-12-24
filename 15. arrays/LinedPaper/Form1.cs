bdf3465e036449686d6146d534fb754866262911
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 15:35:25 +0300

    Задание 3

--- 15. arrays/LinedPaper/Form1.cs
+++ 15. arrays/LinedPaper/Form1.cs
@@ -8,18 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace LinedPaper {
-	public partial class Form1 : Form {
-		public Form1() {
+namespace LinedPaper 
+{
+	public partial class Form1 : Form 
+    {
+		public Form1() 
+        {
 			InitializeComponent();
 		}
 
-		protected override void OnPaint(PaintEventArgs e) {
+		protected override void OnPaint(PaintEventArgs e) 
+        {
 			Graphics graphics = e.Graphics;
 			graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-
-			for (int i = 0; i <= 150; i += 20) {
-				graphics.DrawLine(Pens.LightSkyBlue, 0, i, 300, i); // первая пара чисел - координаты начала отрезка, вторая - координаты конца
+            
+			for (int i = 0; i <= 7; i ++) 
+            {
+				graphics.DrawLine(Pens.LightSkyBlue, 0, i  * 20, 300, i * 20); 
 			}
 		}
 	}
