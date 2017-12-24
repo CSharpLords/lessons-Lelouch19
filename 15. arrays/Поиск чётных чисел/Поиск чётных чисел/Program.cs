0889472b3e18299fcec95a8d4d49d3eaadfdd52a
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 16:21:47 +0300

    Поиск чётных чисел

---
+++ 15. arrays/Поиск чётных чисел/Поиск чётных чисел/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Massif = { 1, 78, 89, -156, 2};
+            for (int n = 0; n < Massif.Length; n = n + 1)
+            {
+                int Rest = Massif[n] % 2;
+                if (Rest == 0)
+                {
+                    Console.WriteLine(Massif[n]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
