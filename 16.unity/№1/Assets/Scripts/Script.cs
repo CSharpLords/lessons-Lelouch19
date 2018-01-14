a97d73c9af313508f50a9cad1cb72fb07f7016bc
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-01-14 17:06:37 +0300

    Unity.Начало

---
+++ 16.unity/№1/Assets/Scripts/Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Script : MonoBehaviour 
+{
+
+	float time = 0;
+	void Start () 
+	{
+	}
+
+	void Update () 
+	{
+		time = time + Time.deltaTime;
+		int seconds =  (int)time;
+		print ("Кол-во секунд - " + seconds);
+	}
+}
