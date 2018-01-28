2cf5be6bebc966e968b60c84347e5472f39b3f30
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-01-28 16:56:41 +0300

    Задание с самолётом

---
+++ 17. unity. conditions/My project 3/Assets/Scripts/High.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class High : MonoBehaviour 
+{
+	double maxHigh = 0;	
+	double minHigh = 1000000;
+	void Update () 
+	{
+		if(transform.position.y > maxHigh)
+		{
+			print ("Максимальная высота " + maxHigh);
+			maxHigh = transform.position.y;
+		}
+		if(transform.position.y < minHigh)
+		{
+			print ("Минимальная высота " + minHigh);
+			minHigh = transform.position.y;
+		}
+	}
+}
