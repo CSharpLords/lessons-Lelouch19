dd248963f557ad5cc7a866631ff9c629bf7554cb
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-01-28 16:04:47 +0300

    Unity.Урок 2

---
+++ 17. unity. conditions/My project 2/Assets/Scripts/EnemyPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyPosition : MonoBehaviour 
+{
+	public Transform monster;
+	void Start () 
+	{
+		float dist = Vector3.Distance(monster.position, transform.position);
+		//print (enemy.position.x);	
+		print(dist);
+	}
+}
