cc6c3afb7224ab694a10921ee751d34d1795434b
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-02-11 16:50:31 +0300

    Светофор

---
+++ 18/Светофор/Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour {
+	float time = 0;
+	Renderer stop;
+	void Start()
+	{
+		stop = GetComponent<Renderer>();
+	}
+	void Update () 
+	{
+		time = time + Time.deltaTime;
+		int seconds = (int)time;
+		if (seconds == 10) 
+		{
+			time = 0;
+		}
+		else if (seconds > 5) 
+		{
+			stop.material.color = Color.white;
+		}
+		else if (seconds < 5 || seconds == 0) 
+		{
+			stop.material.color = Color.red;
+		} 
+
+
+	}
+}
---
+++ 18/Светофор/Assets/Scripts/TrafficLight2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight2 : MonoBehaviour 
+{
+	public float time = 0;
+	Renderer wait;
+	void Start()
+	{
+		wait = GetComponent<Renderer>();
+	}
+	void Update () 
+	{
+		time = time + Time.deltaTime;
+		int seconds = (int)time;
+	    if (seconds == 10) 
+		{
+			time = 0;
+		}
+		else if (seconds > 8) 
+		{
+			wait.material.color = Color.white;
+		}
+		else if(seconds > 5)
+		{
+			wait.material.color = Color.yellow;
+	
+		}
+
+
+	}
+
+}
