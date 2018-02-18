807d1cac04d777a75776e6518df85f4568ee2f6c
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-02-18 16:45:15 +0300

    Движение объекта.Несколько целей

---
+++ 19/движение объекта.Несколько целей/Assets/Scripts/Go.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Go : MonoBehaviour 
+{
+	public Transform[] objects; 
+	float min = 1000;
+	Transform minObj; 
+	void Start () 
+	{
+		for(int x = 0; x < objects.Length; x ++)
+		{
+			float dist =  Vector3.Distance(objects[x].position, transform.position);
+			if(dist < min)
+			{
+				minObj = objects[x];
+				min = dist;
+
+			}
+		}
+	}
+
+	void Update () 
+	{
+		Vector3 direction  = minObj.position - transform.position;
+		direction.Normalize();
+		transform.Translate(direction * 5 * Time.deltaTime);
+	}
+}
