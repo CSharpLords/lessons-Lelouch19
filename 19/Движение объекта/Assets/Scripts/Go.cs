4566053c05f74c1a5db69f188ebd9e53e5af5ee6
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-02-18 15:56:42 +0300

    Движение объекта

---
+++ 19/Движение объекта/Assets/Scripts/Go.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Go : MonoBehaviour {
+	
+	public Transform target; 
+
+	void Start ()
+	{
+		
+	}
+
+	void Update () 
+	{
+		Vector3 direction  = target.position - transform.position;
+		direction.Normalize();
+		transform.Translate(direction * 5 * Time.deltaTime);
+	}
+}
