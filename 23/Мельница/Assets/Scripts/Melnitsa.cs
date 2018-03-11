5f121e89b444da46ca5479da5ab330cee51a1abe
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-03-11 16:24:54 +0300

    Мельница

---
+++ 23/Мельница/Assets/Scripts/Melnitsa.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Melnitsa : MonoBehaviour 
+{
+	bool rotation = true;
+	public Transform palka;
+	public Button startButton;
+
+	void Start()
+	{
+		startButton.onClick.AddListener(Round);
+	}
+	void Update()
+	{
+		if (rotation == true)
+		{
+			palka.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);
+		}
+	}
+    void Round() 
+	{
+		if(rotation == true)
+		{
+			rotation = false;
+		}
+		else
+		{
+			rotation = true;
+		}
+	}
+
+}
