24a3b620b8317002fdad893b82edac5a423f62ae
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-03-18 17:01:48 +0300

    Раннер

---
+++ 24/Раннер/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public Transform hero;
+
+	void Update () 
+	{
+		transform.position = new Vector3 (transform.position.x,transform.position.y,hero.position.z); 
+	}
+}
---
+++ 24/Раннер/Assets/Scripts/HeroControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroControl : MonoBehaviour 
+{
+
+	void Update ()
+	{
+		if (Input.GetKey ("space"))
+		{
+			transform.position = new Vector3(transform.position.x,transform.position.y + 1,transform.position.z);
+		}
+	}
+}
