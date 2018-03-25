74454d672cd14a9b600b13c9a36721ffa64cd4f0
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-03-25 16:31:13 +0300

    AsterisksLine

--- 25. methods/AsteriskLine/Program.cs
+++ 25. methods/AsteriskLine/Program.cs
@@ -1,9 +1,22 @@
 ﻿using System;
-namespace AsteriskLine {
-	class Program {
-		static void Main(string[] args) {
-			PrintAsterisks(10);
+namespace AsteriskLine 
+{
+	class Program 
+    {
+		static void Main(string[] args)
+        {
+			PrintAsterisks(5);
 			Console.ReadLine();
 		}
+        static void PrintAsterisks(int zvezda)
+        {
+            string zvezdochka = "";
+            for (int x = 0; x < zvezda; x++)
+            {
+                zvezdochka = zvezdochka + "*";
+            }
+            Console.WriteLine(zvezdochka);
+
+        }
 	}
 }
