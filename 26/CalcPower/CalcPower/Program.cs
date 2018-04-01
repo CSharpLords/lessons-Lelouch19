e3f984d978dce4d437f4b58d74e4df8dd1f585b2
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-04-01 16:52:28 +0300

    CalcPower

---
+++ 26/CalcPower/CalcPower/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalcPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Введите степень: ");
+            int e = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(n + "^" + e + "=" + Power(n, e));
+            Console.ReadLine();
+        }
+
+        static int Power(int number, int exponent)
+        {
+            int c = 1;
+            for (int x = 0; x < exponent; x++)
+            {
+               c = c * number;
+                
+            }
+            return c;
+        }
+    }
+}
