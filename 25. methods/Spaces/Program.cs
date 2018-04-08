1d9aeb9bfbca5035e145186b8566b3414eaa6b2c
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-04-08 16:03:12 +0300

    Spaces

--- 25. methods/Spaces/Program.cs
+++ 25. methods/Spaces/Program.cs
@@ -2,13 +2,29 @@
 
 namespace Spaces {
 	class Program {
-		static void Main(string[] args) {
+		static void Main(string[] args) 
+        {
 			Console.Write("Введите предложение: ");
 			string sentence = Console.ReadLine();
 
-			int spacesAmount = CountSpaces(sentence);
+		    int spacesAmount = CountSpaces(sentence);
 			Console.Write("Предложение '" + sentence + "' содержит " + spacesAmount + " пробелов");
 			Console.ReadLine();
 		}
+
+        static int CountSpaces(string sentence)
+        {
+            int spacesAmount = 0;
+            for (int x = 0; x < sentence.Length; x ++ )
+            {
+                char space = ' ';
+                if(sentence[x] == space)
+                {
+                    spacesAmount = spacesAmount + 1;
+                }
+            }
+            return spacesAmount;
+        }
+
 	}
 }
