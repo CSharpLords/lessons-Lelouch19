743f5312f9eca185d3710660d08bad12e9f8b2ec
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-04-08 16:18:26 +0300

    UpgradeSpaces

--- 25. methods/UpgradedSpaces/Program.cs
+++ 25. methods/UpgradedSpaces/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 
-namespace UpgradedSpaces {
-	class Program {
-		static void Main(string[] args) {
+namespace UpgradedSpaces 
+{
+	class Program 
+    {
+		static void Main(string[] args) 
+        {
 			Console.Write("Введите предложение: ");
 			string sentence = Console.ReadLine();
 
@@ -11,5 +14,37 @@ namespace UpgradedSpaces {
 			Console.Write("Предложение '" + sentence + "' содержит " + spacesAmount + " " + spacesForm);
 			Console.ReadLine();
 		}
+        static int CountSpaces(string sentence)
+        {
+            int spacesAmount = 0;
+            for (int x = 0; x < sentence.Length; x++)
+            {
+                char space = ' ';
+                if (sentence[x] == space)
+                {
+                    spacesAmount = spacesAmount + 1;
+                }
+            }
+            return spacesAmount;
+        }
+
+        static string GetSpacesForm(int spaceAmount)
+        {
+            string spacesForm = "";
+            if(spaceAmount == 1)
+            {
+                spacesForm = "пробел.";
+            }
+            else if(spaceAmount > 1 && spaceAmount < 5)
+            {
+                spacesForm = "пробела.";
+            }
+            else
+            {
+                spacesForm = "пробелов.";
+            }
+            return spacesForm;
+        }
+
 	}
 }
