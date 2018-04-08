8434e9ef461ed3bcb7a5444167268fb7621c6445
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-04-08 16:46:15 +0300

    WordInBox

---
+++ 27/WordInBox/WordInBox/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово:");
+            string text = Console.ReadLine();
+            WriteUnder(text);
+
+            Console.ReadLine();
+        }
+        static void WriteUnder(string text)
+        {
+            string symbols = "";
+            for (int x = 0; x < text.Length; x ++)
+            {
+                symbols = symbols + "-";
+            }
+            Console.WriteLine("-" + symbols + "-");
+            Console.WriteLine("-" + text + "-");
+            Console.WriteLine("-" + symbols + "-");
+        }
+    }
+}
