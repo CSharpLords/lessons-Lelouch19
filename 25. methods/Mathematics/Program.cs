7a39b49e0f65c8d6508dd8067eaf97bbaf5a7e36
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-04-08 16:24:15 +0300

    Mathematics

--- 25. methods/Mathematics/Program.cs
+++ 25. methods/Mathematics/Program.cs
@@ -2,9 +2,21 @@
 
 namespace Mathematics {
 	class Program {
-		static void Main(string[] args) {
+		static void Main(string[] args) 
+        {
 			Console.WriteLine(Sum(3, Multiply(7, 9)));
 			Console.ReadLine();
 		}
+
+        static int Multiply(int number1, int number2)
+        {
+            int number = number1 * number2;
+            return number;
+        }
+        static int Sum(int number1,int number2)
+        {
+            int number = number1 + number2;
+            return number;
+        }
 	}
 }
