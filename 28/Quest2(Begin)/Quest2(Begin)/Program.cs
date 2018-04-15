7cc1c65df3d29a5d92495b76c6a52ba62b789427
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-04-15 18:10:59 +0300

    quest

---
+++ 28/Quest2(Begin)/Quest2(Begin)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+
+    class Program
+    {
+        static double health = 100;
+        static double damage = 30;
+        static void Main(string[] args)
+        {
+            Torgash();
+        }
+        static void Torgash()
+        {
+            Console.WriteLine("'Привет,путник!Я торговец Владимир,продайте мне своего коня!'");
+            Console.ReadLine();
+
+        }
+
+    }
+}
