f564c1175c9bfffc7de0d56172b2831916255556
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-05-06 17:01:02 +0300

    SpacePlanes

---
+++ 31/SpacePlane/SpacePlane/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rend = new Random();
+            SpacePlane[] spacePlanes = new SpacePlane[10];
+
+            for (int i = 0; i < spacePlanes.Length; i++)
+            {
+                spacePlanes[i] = new SpacePlane(rend.Next(1, 5));
+            }
+
+            while (true)
+            {
+                for (int i = 0; i < spacePlanes.Length; i++)
+                {
+                    spacePlanes[i].Fly();
+                    if(spacePlanes[i].dist.Length == 12)
+                    {
+                        Console.WriteLine("Финиш!!!");
+                        break;
+                    }
+                }
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ 31/SpacePlane/SpacePlane/SpacePlane.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class SpacePlane
+    {
+         public string speed = "";
+         public string dist = "";
+         public SpacePlane(int speedX)
+         {      
+            for (int i = 0; i < speedX; i++)
+            {
+                speed = speed + "-";
+            }
+         }
+         public void Fly()
+         {
+             dist = dist + speed;
+             Console.WriteLine(dist);
+         }
+
+    }
+}
