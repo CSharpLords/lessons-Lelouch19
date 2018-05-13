cf5ce6421e7dae5624208b851b4db94c2d868ad9
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-05-13 16:47:07 +0300

    AirAttack

---
+++ 32/AirAttack/AttackPlane/Bomber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AirAttack
+{
+    class Bomber
+    {
+        static int x = 0;
+        public string plane = "-->";
+        public int speed = 100;
+        public void Draw()
+        {
+
+                int end = Console.WindowWidth;
+                x = x + 3;
+                if (end - 1 < x)
+                {
+                    x = 0;
+                }
+                Console.SetCursorPosition(x, 0);
+                Console.WriteLine(plane);
+        }
+    }
+}
---
+++ 32/AirAttack/AttackPlane/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AirAttack
+{
+    class Program
+    {
+        //https://www.omfgdogs.com/#
+        //https://github.com/CSharpWizards/lessons_w-pchuck77/tree/master/13.%20classes
+        static void Main(string[] args)
+        {
+            int end = Console.WindowWidth;
+            while (true)
+            {
+                Update();
+                Thread.Sleep(100);
+                Console.Clear();
+            }
+        }
+        static void Update()
+        {
+            Bomber plane = new Bomber();
+            plane.Draw();
+        }
+    }
+}
